1befb4c148052be08431be6f43d29def4cc11547
Jordi Irawan <64052045+jrdrwn@users.noreply.github.com>
2024-09-27 22:01:07 +0700

    Latihan Implementasi Library Glide

--- app/src/main/java/com/dicoding/myrecyclerview/ListHeroAdapter.kt
+++ app/src/main/java/com/dicoding/myrecyclerview/ListHeroAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 class ListHeroAdapter(private val listHero: ArrayList<Hero>) :
     RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
@@ -25,7 +26,7 @@ class ListHeroAdapter(private val listHero: ArrayList<Hero>) :
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
         val (name, description, photo) = listHero[position]
-        holder.imgPhoto.setImageResource(photo)
+        Glide.with(holder.itemView.context).load(photo).into(holder.imgPhoto)
         holder.tvName.text = name
         holder.tvDescription.text = description
 
